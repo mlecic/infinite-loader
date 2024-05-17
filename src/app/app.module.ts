6c92867f0f9fb7a1db788fce6aec4d2016e0d44d
Mirko Lecic <lecic.mirko@gmail.com>
2024-05-17 23:36:08 +0200

    Created Dashboard and List components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
