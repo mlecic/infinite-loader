3a1e68d2a6b228b3a0ae5d5f96e49c025f484198
Mirko Lecic <lecic.mirko@gmail.com>
2024-05-18 13:40:40 +0200

    Added GoBack button

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { PhotosService, Photo } from '../photos.service';
 
 @Component({
@@ -10,12 +11,16 @@ export class ListComponent implements OnInit {
 
   photos: Photo[] = [];
 
-  constructor(private photosService: PhotosService) {}
+  constructor(private router: Router, private photosService: PhotosService) {}
 
   ngOnInit(): void {
     this.photosService.getPhotos(1, 10).subscribe(response => {
       this.photos = response.body as Photo[];
     });
+  }
+
+  goBack() {
+    this.router.navigateByUrl("/");
   }
 
 }
