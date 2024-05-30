f9c068933d07a67a00b47d5b20ad0c4002628e42
Mirko Lecic <lecic.mirko@gmail.com>
2024-05-30 20:58:48 +0200

    Added test for the service

--- src/app/photos.service.spec.ts
+++ src/app/photos.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { PhotosService } from './photos.service';
+import { Photo } from './photos.model';
+import { photo1, photo2 } from './state/photos.mock';
+import { of } from 'rxjs';
 
-xdescribe('PhotosService', () => {
-  let service: PhotosService;
+let httpClientSpy: jasmine.SpyObj<HttpClient>;
+let photosService: PhotosService;
+
+describe('PhotosService', () => {
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PhotosService);
+
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    photosService = new PhotosService(httpClientSpy);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should return expected photos', (done: DoneFn) => {
+    const expectedPhotos: Photo[] = [
+      photo1,
+      photo2
+    ]
+
+    httpClientSpy.get.and.returnValue(of({ body: expectedPhotos } as HttpResponse<Photo[]>));
+
+    photosService.getPhotos().subscribe(response => {
+      expect(response.body).withContext('expected photos').toEqual(expectedPhotos);
+      done();
+    })
   });
 });
